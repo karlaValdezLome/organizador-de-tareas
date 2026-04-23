eb0c9db91c3b90a1ec082cdb96bcd622beaacc5c
karlaValdezLome <24308060610098@cetis61.edu.mx>
2026-04-23 07:15:46 -0500

    vali de usuario

--- organizador de tareas/app.py
+++ organizador de tareas/app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
+usuarios = {
+    "admin": "karla",
+    "usuario": "contraseña123"
+}
 
-@app.route('/iniciodesecion')
+@app.route('/iniciodesecion', methods=['GET', 'POST'])
 def iniciodesecion():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in usuarios and usuarios[username] == password:
+            return redirect(url_for('index'))
+        else:
+            return render_template('iniciodesecion.html', error="Usuario o contraseña incorrectos")
     return render_template('iniciodesecion.html')
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 @app.route('/recuperar_contraseña')
 def recuperar_contraseña():
     return render_template('recuperar_contraseña.html')
-
 
 @app.route('/registro')
 def registro():
